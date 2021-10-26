1966f1573e46004c2d1940783a0105d83ad86cff
Vasil Bit <vasil.bituni@gmail.com>
2021-10-26 11:38:41 +0200

    Make hero image responsive

--- src/Home/HeroSection.tsx
+++ src/Home/HeroSection.tsx
@@ -2,31 +2,36 @@ import React from 'react';
 import classes from './HeroSection.module.css';
 import MediaQuery from 'react-responsive';
 import Button from '../shared/Button';
+//import Background from '../images/seafood.jpg';
 
 const HeroSection: React.FC = () => {
   return (
-    <div className={classes.grid}>
-      <div className={classes.containerLeft}>
-        <h1 className={classes.title}>
-          Experience Saranda as a <strong>local</strong>{' '}
-        </h1>
-        <p className={classes.subtitle}>
-          Experience Saranda as a local. Avoid tourist-traps and find the best
-          reccomandations out there Experience Saranda as a local. Avoid
-          tourist-traps and find the best reccomandations out there est
-          reccomandations out there
-        </p>
-        <div className={classes.containerButtons}>
-          <Button buttonStyle={'btn--main'}>Explore</Button>
-          <Button buttonStyle={'btn--outline'}>Read More</Button>
+    <div className={classes.container}>
+      <div className={classes.grid}>
+        <div className={classes.containerLeft}>
+          <div className={classes.gridLeft}>
+            <h1 className={classes.title}>
+              Experience Saranda as a <strong>local</strong>{' '}
+            </h1>
+            <p className={classes.subtitle}>
+              Experience Saranda as a local. Avoid tourist-traps and find the
+              best reccomandations out there Experience Saranda as a local.
+              Avoid tourist-traps and find the best reccomandations out there
+              est reccomandations out there
+            </p>
+            <div className={classes.containerButtons}>
+              <Button buttonStyle={'btn--main'}>Explore</Button>
+              <Button buttonStyle={'btn--outline'}>Read More</Button>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <MediaQuery minWidth={1000}>
-        <div className={classes.containerRight}>
-          <img src={'./images/background2.jpg'} alt="sailing-boat" />
-        </div>
-      </MediaQuery>
+        <MediaQuery minWidth={1300}>
+          <div className={classes.backgroundContainer}>
+            <div className={classes.imageContainer}></div>
+          </div>
+        </MediaQuery>
+      </div>
     </div>
   );
 };
